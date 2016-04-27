11be16c9ede50706cd7641f7e9e1e3f285ca244a
Mike Wickwar <mike.wickwar@gmail.com>
2016-04-26 19:16:12 -0600

    starting styling decisions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 console.log('jq workin like yo mama');
 
 var switchposition = 2;
+$("#switchLabel").html("inactive");
 
 $("#handle").click(function() {
   console.log('click click');
@@ -8,18 +9,28 @@ $("#handle").click(function() {
   switch (switchposition) {
     case switchposition = 1:
       switchposition = 3;
+      $("#switchLabel").empty();
+      $("#switchLabel").append("<h3>ON</h3>");
+      $("#handle").css("background-image", "-webkit-radial-gradient(center, circle cover, #00C429, #009A0C)");
       $(this).animate({
             'marginLeft' : "+=80px" //moves to on
             })
       break;
     case switchposition = 2:
       switchposition = 3;
+      $("#switchLabel").empty();
+      $("#switchLabel").append("<h3>ON</h3>");
+      $("#handle").css("background-image", "-webkit-radial-gradient(center, circle cover, #00C429, #009A0C)");
       $(this).animate({
             'marginLeft' : "+=40px" //moves to on
+
             })
       break;
     case switchposition = 3:
       switchposition = 1;
+      $("#switchLabel").empty();
+      $("#switchLabel").append("<h3>OFF</h3>");
+      $("#handle").css("background-image", "-webkit-radial-gradient(center, circle cover, #FF0C00, #D32219)");
       $(this).animate({
             'marginLeft' : "-=80px" //moves to off
             })
