dc6247c07e680f95c9dc2334b5dd3737d15aa777
Mike Wickwar <mike.wickwar@gmail.com>
2016-05-06 14:06:46 -0600

    ready for deployment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log('jq workin like yo mama');
 
-var switchposition = 2;
+var switchposition = 1;
 $("#switchLabel").html("inactive");
 
 $("#handle").click(function() {
@@ -11,29 +11,17 @@ $("#handle").click(function() {
       switchposition = 3;
       $("#switchLabel").empty();
       $("#switchLabel").append("<h3>ON</h3>");
-      $("#handle").css("background-image", "linear-gradient(#83c783 0%, #52b152 40%, #008a00 100%)")
-                  .css("box-shadow", "inset 0px 1px 1px #b4ddb4, 0px 1px 3px #b4ddb4");
-      $(this).animate({ 'marginLeft' : "+=80px" })
+      $(this).animate({ 'marginLeft' : "+=40px"})
+      $("#divColor").animate({ 'opacity' : "1"})
       break;
-    case switchposition = 2:
-      switchposition = 3;
-      $("#switchLabel").empty();
-      $("#switchLabel").append("<h3>ON</h3>");
-      $("#handle").css("background", "#fcfff4")
-                  .css("background", "linear-gradient(#83c783 0%, #52b152 40%, #008a00 100%)")
-                  .css("box-shadow", "inset 0px 1px 1px #b4ddb4, 0px 1px 3px #b4ddb4");
-      $(this).animate({
-            'marginLeft' : "+=40px" //moves to on
-             })
-      break;
+
     case switchposition = 3:
       switchposition = 1;
       $("#switchLabel").empty();
       $("#switchLabel").append("<h3>OFF</h3>");
-      $("#handle").css("background", "#c0c0c0");
-      $(this).animate({
-            'marginLeft' : "-=80px" //moves to off
-            })
+      $(this).animate({ 'marginLeft' : "-=40px"})
+      $("#divColor").animate({ 'opacity' : "0"})
+
       break;
 
   }
